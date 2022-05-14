1ae017b29cbb3d958d90ff0263d431880073bc61
Elouan <callec.elouan29@gmail.com>
2022-05-14 16:30:54 +0200

    création du formulaire de connexion

--- GSB_frais/formulaire_connexion.php
+++ GSB_frais/formulaire_connexion.php
@@ -1,3 +1,8 @@
+<!-- Connexion a la base de donnees -->
+<?php
+include "connexionBDD.php"
+?>
+
 <!doctype html>
 <html>
 
@@ -8,17 +13,49 @@
 </head>
 
 <body>
+
     <!-- Bande en haut de page -->
     <div class="HP">
         <p>Identification</p>
         <img src="style/gsb logo.png" class="logo">
     </div>
 
-    <form method="POST" action="formulaire_connexion.html" class="smallForm">
+
+    <?php
+
+    // Récupération du login et password du formulaire
+    if (isset($_POST['login'])) {
+        $login = $_POST['login'];
+    }
+    if (isset($_POST['password'])) {
+        $password = $_POST['password'];
+    }
+
+    // Requête pour rechercher le login et password du visiteur
+    if (isset($login) and isset($password)) {
+        $req = $bdd->prepare('SELECT login, mdp, prenom FROM visiteurmedical WHERE login = :login AND mdp = :password');
+        $req->execute(array(
+            'login' => $login,
+            'password' => $password
+        ));
+        $donnees = $req->fetch();
+    }
+
+    // Verification du login et du mot de passe
+    if (isset($donnees)) :
+        if ($login === $donnees['login'] and $password === $donnees['mdp']) {
+            header('Location:interface.php');
+        } else echo '<h2>Mauvais identifiant ou mot de passe !</h2>';
+    endif;
+
+    ?>
+
+
+    <form method="POST" action="formulaire_connexion.php" class="smallForm">
         <p class="titreFormulaire">Connexion</p>
         <div class="centre">
             <p>
-                <label>Login : </label>
+                <label>Identifiant : </label>
                 <input name="login" id="login" value="" type="text" class="case" />
             </p>
         </div>
