977fa98506c5f3d5c57cba3fb822f793b7f9ec68
Elouan <callec.elouan29@gmail.com>
2022-05-16 23:23:24 +0200

    page de modification fonctionnelle

--- GSB_frais/modifFicheFrais.php
+++ GSB_frais/modifFicheFrais.php
@@ -8,130 +8,112 @@ session_start();
 <html>
 
 <head>
-    <title>Gestion de frais</title>
+    <title>Gestion des frais</title>
     <meta charset="utf-8" />
     <link href="style/style.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
 
-<?php
+    <?php
 
-/* On regarde si on est dans le cas d'une modification */
-if ( isset($_GET['BOmodif'] ) ==true)
-{
-    $monTraitement = "Update.php";		/* Traitement SQL à appeler après la FORM */
-    $monAction = "Modif";
+    // Recuperation des valeurs de fichefrais
+    $req = $bdd->prepare("SELECT id, idVisiteur, mois, annee, nbJustificatifs, montantValide, dateModif, idEtat FROM fichefrais
+                          WHERE id = :id");
 
-    /* Chercher dans la base les valeurs à modifier */
-    $cnxBDD = connexion();
+    $req->execute(array('id' => $_GET['id']));
+    $donnees = $req->fetch();
 
-    $sql = "SELECT id, idVisiteur, mois, annee, nbJustificatifs, montantValide  , dateModif,idEtat
-             FROM fichefrais
-             WHERE id = ".$_GET['id'].";";
+    // Declaration des variables
+    $identifiantFicheFrais = $donnees['id'];
+    $idVisiteur = $donnees['idVisiteur'];
+    $mois = $donnees['mois'];
+    $annee = $donnees['annee'];
+    $nbJustificatifs = $donnees['nbJustificatifs'];
+    $montantValide = $donnees['montantValide'];
+    $dateModif = $donnees['dateModif'];
+    $idEtat = $donnees['idEtat'];
 
-    $nuitees=0;
-    $repasmidi=0;
-    
-    $result = $cnxBDD->query($sql);
-    $maLigne = $result->fetch_assoc();
-    $identifiant = $maLigne['id'];
-    $idVisiteur = $maLigne['idVisiteur'];
-    $mois= $maLigne['mois'];
-    $annee= $maLigne['annee'];
-    $montantValide=$maLigne['montantValide'];
-    $nbJustificatifs= $maLigne['nbJustificatifs'];
-    $dateModif = $maLigne['dateModif'];
-    $idEtat = $maLigne['idEtat'];
+    // Recuperation des valeurs de lignefraisforfait
+    $req = $bdd->prepare("SELECT idForfait, quantite FROM lignefraisforfait 
+                          WHERE idFicheFrais= :id");
 
-    $get_data="SELECT idForfait, quantite 
-                FROM lignefraisforfait 
-                WHERE idFicheFrais=".$_GET['id'].";";
-    $get_data=$cnxBDD -> query($get_data);
-    
-    while ($elements=$get_data -> fetch_assoc()){     
-        switch ($elements['idForfait']){
+    $req->execute(array('id' => $_GET['id']));
+
+
+    while ($elements = $donnees = $req->fetch()) {
+        switch ($elements['idForfait']) {
             case 'NUI':
-                $nuitees=$elements['quantite'];
+                $nuitees = $elements['quantite'];
                 break;
             case 'REP':
-                $repasmidi=$elements['quantite'];
+                $repasmidi = $elements['quantite'];
                 break;
-            /*case 'ETA' :
-                $etape=$elements['quantite'];
+            case 'ETA':
+                $etape = $elements['quantite'];
                 break;
-            case 'KM' : 
-                $km=$elements['quantite'];
-                break;*/
+            case 'KM':
+                $km = $elements['quantite'];
+                break;
         }
     }
-}
-else
-{
-    /* Cas d'une insertion */
-    $monTraitement = "insert.php";	/* Traitement SQL à appeler après la FORM */
-    $monAction = "Insert";
-    
-    $identifiant = '';
-    $idVisiteur = '';
-    $mois = '';
-    $annee = '';
+    ?>
 
-    $montantValide = '';
-    $repasmidi = '';
-    $nuitees = '';
-    /*$etape='';
-    $km = '';*/
-}
-?>
-	        
-    <div class="couleurecriture">
-    
-        <p ><h1>Saisie</h1></p>
+    <!-- Bande en haut de page -->
+    <div class="HP">
+        <p>Gestion des Frais</p>
+        <img src="style/gsb logo.png" class="logoHP">
+    </div>
 
-            <form id="formulaire" action="<?php echo $monTraitement; ?>" method="GET">
-                <h2>PERIODE D'ENGAGEMENT : </h2>
-                
+    <div>
+        <p class="titrePage" id="titrePageGestionFrais">Saisie</p>
+        <br />
+    </div>
+
+    <form method="POST" id="formulaire" action="update.php">
+        <input type="hidden" value="<?php echo $_GET['id']; ?>" name="idFicheFrais">
+        <section>
+            <div class="gauche" id="gaucheGestionFrais">
+                <p>PERIODE D'ENGAGEMENT :</p>
+            </div>
+            <div class="milieu" id="milieuGestionFrais">
                 <label for="mois">Mois (2 chiffres) : </label>
-                <input type="text" id="mois" name="mois" value= "<?php echo $mois; ?>"/>
+                <input type="text" id="mois" name="mois" value="<?php echo $mois; ?>" />
 
                 <label for="annee">Années (4 chiffres) : </label>
-                <input type="text" id="annee" name="annee" value= "<?php echo $annee; ?>"/>            
-                
-                
-                
-                <p><h3>Frais au forfait : </h3></p>
-                
-                
-                <input type="hidden" id="id" name="id" value= "<?php echo $_GET['id']; ?>"/>
-                
+                <input type="text" id="annee" name="annee" value="<?php echo $annee; ?>" />
+            </div>
+        </section>
 
-                <div>
-                    <label for="repasmidi">Repas midi : </label>
-                    <input type="text" id="repasmidi" name="repasmidi" value= "<?php echo $repasmidi; ?>"/>
-                    
-                </div>
-                <br>
-                <div>
-                    <label for="nuitees">Nuitées : </label>
-                    <input type="text" id="nuitees" name="nuitees" value= "<?php echo $nuitees; ?>"/>
-                </div>
-                <br>
-                <!--<div>
-                    <label for="etape">Etape : </label>
-                    <input type="text" id="etape" name="etape" value= "<?php echo $etape; ?>"/>
-                </div>
-                <br>
-                <div>
-                    <label for="km">Km : </label>
-                    <input type="text" id="km" name="km" value= "<?php echo $km; ?>"/>
-                </div>-->
-                <br/>
-                
-                <div>
-                    <input type="submit">
-                </div>
-            </form>
+        <br />
+
+        <section>
+            <h1>Frais au forfait</h1>
+
+            <div>
+                <label for="repasmidi">Repas midi :</label>
+                <input type="text" id="repasmidi" name="repasmidi" value="<?php echo $repasmidi; ?>" />
+            </div>
+            <div>
+                <label for="nuitees">Nuitées :</label>
+                <input type="text" id="nuitees" name="nuitees" value="<?php echo $nuitees; ?>" />
+            </div>
+            <div>
+                <label for="etape">Etape :</label>
+                <input type="text" id="etape" name="etape" value="<?php echo $etape; ?>" />
+            </div>
+            <div>
+                <label for="km">Km :</label>
+                <input type="text" id="km" name="km" value="<?php echo $km; ?>" />
+            </div>
+            <br />
+        </section>
+
+        <div>
+            <input type="submit" value="Soumettre la requête" class="bouton">
+        </div>
+    </form>
     </div>
 </body>
+
 </html>
